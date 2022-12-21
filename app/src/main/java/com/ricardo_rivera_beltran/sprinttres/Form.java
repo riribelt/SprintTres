4f2d26358943b685f55076e817a0c2e259bc9457
ricardo-mt2022 <riribelt@yahoo.es>
2022-12-21 10:23:20 -0500

    Initial commit

---
+++ app/src/main/java/com/ricardo_rivera_beltran/sprinttres/Form.java
@@ -0,0 +1,14 @@
+package com.ricardo_rivera_beltran.sprinttres;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Form extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+    }
+}
